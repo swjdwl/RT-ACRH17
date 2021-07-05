1684247996c8393186d01c2ef56e9b2bb2bba335
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2021-07-05 19:56:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git webrestriction lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-sed -i '$a src-git fileassistant lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
