27b6ea9e8498d48a8876f3edbeb745338f3d8eec
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2021-07-17 10:54:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 #删除原来的argon
 rm -rf  package/lean/luci-theme-argon
