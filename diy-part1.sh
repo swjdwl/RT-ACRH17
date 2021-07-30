ecebff7b2e322612417e298f5ffded20134c91af
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2021-07-30 23:25:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,6 @@
 
 # Add a feed source
 
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default   
-#这是passwall插件的最新库地址，库名、包名、菜单名目前统一，之前菜单名是 科学上网、...还有什么名字瞎几把改的我忘了...
-
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
