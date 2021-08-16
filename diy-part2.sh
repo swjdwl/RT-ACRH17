0c1b0bf7672a45a328ecc5eba84f6d71e593db7a
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2021-08-16 13:07:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,13 +14,13 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 #### 修改主机名字，把YOU-R4A修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' package/lean/default-settings/files/zzz-default-settings
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' package/lean/default-settings/files/zzz-default-settings
 
 #### 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /PandoraBox /g" package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/OpenWrt /PandoraBox /g" package/lean/default-settings/files/zzz-default-settings
 
 #### 稳定版修改R21xxx+自己的名字
-sed -i 's/R21.8.6/情人节快乐版/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R21.8.6/LSP 2021.08.16/g' package/lean/default-settings/files/zzz-default-settings
 
 #删除原来的argon
 rm -rf  package/lean/luci-theme-argon
