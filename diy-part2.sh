4cebe247e63b77d516c145919c79f9a5e086543a
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2021-10-21 09:30:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' pa
 sed -i "s/OpenWrt /PandoraBox /g" package/lean/default-settings/files/zzz-default-settings
 
 #### 稳定版修改R21xxx+自己的名字
-sed -i 's/R21.10.1/R21.10.20/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R21.10.1/R21.10.21/g' package/lean/default-settings/files/zzz-default-settings
 
 sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio0.ssid=RT-ACRH17/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
