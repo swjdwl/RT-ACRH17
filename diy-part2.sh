12edb3c860d7e2a82e649b3e5c60b7f344843af7
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2021-12-18 13:00:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,9 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-rm -rf  package/lean/luci-theme-argon
+rm -rf  package/lean/luci-theme-argon-18.06
 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-18.06
 
 #### 修改主机名字，把YOU-R4A修改你喜欢的就行（不能纯数字或者使用中文）
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' package/lean/default-settings/files/zzz-default-settings
