b81f893fdea8894c06bd9b06798a2d68fed814b4
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-03-04 20:58:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,8 @@
 
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
