b2aa9a06e5f3f635be3d3c87b5722f1eee864ab2
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-03-25 15:13:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,4 +33,4 @@ sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luc
 
 sed -i 's/"CPU 性能优化调节"/"处理器性能优化调节设置"/g' feeds/luci/applications/luci-app-cpufreq/po/zh-cn/cpufreq.po
 
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git feeds/luci/applications/luci-app-unblockneteasemusic
